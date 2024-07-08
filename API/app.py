81e6a51d17a5b11ed37a99e2b2f9c63d73715e78
3swayam <3swayam@gmail.com>
2024-07-08 14:12:26 +0530

    Refactored

--- API/app.py
+++ API/app.py
@@ -1,80 +1,21 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
+from cost_calculator import CostCalculator
 
 app = Flask(__name__)
 CORS(app)  # Enable CORS for all routes
 
-def calculate_each_section(eachItem):
-    iteration_cost_per_section = 0
-    monthly_cost_per_section = 0
-    fixed_cost_per_section = 0
-
-    for price in eachItem["spec"]:
-        # Get inputs for cost & service Type
-        isFixed = price['isFixed']
-        isFaaS = price['isFaas']
-
-        # Case1: Monthly FAAS - cost/iteration
-        if not isFixed and isFaaS:
-            cost_per_month = price['price']
-            no_of_iteration_monthly = price['noOfIteration']
-            if no_of_iteration_monthly != 0:
-                price['iterationCost'] = (cost_per_month / no_of_iteration_monthly)
-            else:
-                price['iterationCost'] = 0  # Avoid division by zero
-            # Adding to section cost
-            iteration_cost_per_section = iteration_cost_per_section + price['iterationCost']
-
-        elif isFixed and isFaaS:
-            # Case2: Fixed FAAS   - cost/month and cost/iteration
-            if price['name'] in ["Development", "Training"]:
-                price['fixedCost'] = price['price']
-                fixed_cost_per_section = fixed_cost_per_section + price['fixedCost']
-            else:
-                fixed_cost = price['price']
-                no_of_iterations = price['noOfIteration']
-                if no_of_iterations != 0:
-                    price['iterationCost'] = fixed_cost / no_of_iterations
-                else:
-                    price['iterationCost'] = 0  # Avoid division by zero
-                price['monthlyCost'] = fixed_cost
-
-                # Yearly cost & yearly iteration no, to be divided by fixed cost per year
-                monthly_cost_per_section = monthly_cost_per_section + (price['monthlyCost'])
-                iteration_cost_per_section = iteration_cost_per_section + price['iterationCost']
-
-        else:
-            # Case3 and Case4: Fixed PaaS  and Monthly PaaS  - cost/month
-            # value entered is monthly cost by default
-            price['monthlyCost'] = price['price']
-            # Adding to section cost
-            monthly_cost_per_section = monthly_cost_per_section + price['monthlyCost']
-
-    return eachItem, iteration_cost_per_section, monthly_cost_per_section, fixed_cost_per_section
+# Create an instance of the CostCalculator class
+cost_calculator = CostCalculator()
 
 @app.route('/add', methods=['POST'])
 def add():
     # Extract JSON payload from the request
     data = request.get_json()
 
-    # Start calculation
-    total_Cost_per_iteration = 0
-    total_Cost_per_month = 0
-    total_Fixed_cost = 0
+    # Call the calculate_cost function from CostCalculator class
+    costs = cost_calculator.calculate_cost(data)
 
-    for eachItem in data:
-        eachItem, iteration_cost, monthly_cost, fixed_cost = calculate_each_section(eachItem)
-        total_Cost_per_iteration = total_Cost_per_iteration + iteration_cost
-        total_Cost_per_month = total_Cost_per_month + monthly_cost
-        total_Fixed_cost = total_Fixed_cost + fixed_cost
-
-    # Return the JSON payload as a response
-    costs = {
-        'total_Cost_per_iteration': total_Cost_per_iteration,
-        'total_Cost_per_month': total_Cost_per_month,
-        'total_Fixed_cost': total_Fixed_cost,
-        'data': data,
-    }
     return jsonify(costs)
 
 if __name__ == '__main__':
